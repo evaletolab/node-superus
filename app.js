87a8ab837bdfc0050d73a89a0c799ec122e33ff8
evaleto <evaleto@gmail.com>
2012-06-05 19:57:24 +0200

    atom feed issue

--- app.js
+++ app.js
@@ -13,13 +13,11 @@ var express = require('express')
 var app = module.exports = express.createServer();
 
 
-//
-// manage local or c9 env.
 var port = (process.env.VMC_APP_PORT || process.env.C9_PORT || 3000);
-var host = (process.env.HOST || 'localhost');
+var host = (process.env.VMC_APP_HOST || 'localhost');
 
 //
-// manage vmc env
+// manage c9 env
 if (process.env.C9_PORT ){
     host='0.0.0.0';
 }
